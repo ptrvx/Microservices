fb8c3e7f7e994743e739235b6db79f62dacf2e79
ptrvx <petar.vukovic@hotmail.com>
2019-12-03 19:09:55 +0100

    Zuul + Eureka setup

---
+++ Zuul/src/test/java/rs/edu/raf/msa/Zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.edu.raf.msa.Zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
